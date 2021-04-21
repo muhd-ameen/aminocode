d874c071628fc7f80d656d3332ce74db64bd5f96
muhd ameen <ameens.in19@gamil.com>
2021-04-21 11:16:12 +0530

    portfolio completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,68 @@
+$(document).ready(function() {
 
-$(document).ready(function () {
-
-    $("#name").blur(function () {
-        var text = $(this).val()
-        if (text.length < 4) {
-            $("#err1").show()
-
-        } else {
-            $("#err1").hide()
-
-        }
-        var text1 = $(this).val()
-        if (/^[a-zA-Z]+$/.test(text1) == false) {
+        $("#name").blur(function (){
+            var text=$(this).val()
+            if (text.length<4) {
+                $("#err1").show()
+                flag1=1;
+                if(flag1==1){
+                    $("#submitbtn").prop("disabled", true);
+                }
+            
+            } else {
+                $("#err1").hide()
+                flag1=0;
+                $("#submitbtn").prop("disabled", false);
+            }
+        })
+        
+    $("#emailinput").blur(function(){
+    
+        var inputvalues=$(this).val()
+        if(/^[a-zA-Z0-9-.]+\@[a-zA-Z]+\.[a-zA-Z]+$/.test(inputvalues)==false){
             $("#err2").show()
-
-        } else {
+            flag2=1;
+            if(flag2==1){
+                $("#submitbtn").prop("disabled", true);
+            }
+        }else{
             $("#err2").hide()
-
+            flag2=0;
+            $("#submitbtn").prop("disabled", false);
+    
         }
     })
-
-    $("#email").blur(function () {
-
-        var inputvalues = $(this).val()
-        if (/^[a-zA-Z0-9-.]+\@[a-zA-Z]+\.[a-zA-Z0-9]+$/.test(inputvalues) == false) {
-            $("#err2").show()
-
+    
+    $("#numberinput").blur(function (){
+        var text2=$(this).val()
+        if (text2.length<10) {
+            $("#err3").show()
+            flag3=1;
+            if(flag3==1){
+                $("#submitbtn").prop("disabled", true);
+            }
         } else {
-            $("#err2").hide()
-
+            $("#err3").hide()
+            flag3=0;
+            $("#submitbtn").prop("disabled", false);
+    
         }
+    
+        var text2=$(this).val()
+        if(/^\d{10}$/.test(text2)==false){
+            $("#err3").show()
+            flag4=1;
+            if(flag4==1){
+                $("#submitbtn").prop("disabled", true);
+            }
+        }else{
+            $("#err3").hide()
+            flag4=0;
+            $("#submitbtn").prop("disabled", false);
+    
+        }
+    
+        
     })
 
 })
